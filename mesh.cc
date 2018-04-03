fcd7fec4edb37c22521caf23a0298a49abf6a045
Chun-Yu Ke <ck659@cornell.edu>
2018-04-02 20:32:17 -0400

    define radius as variable

--- mesh.cc
+++ mesh.cc
@@ -3,12 +3,12 @@
 #include <iostream>
 using namespace std;
 
+const double rr = 25;
 const double rout = 745, rin = 395, size = 87.5;
-const double z_min = size / 2, z_max = 600;
+const double z_min = rr, z_max = 10000 + rr;
 const double delta_t = M_PI / 6, delta_r = size, delta_z = size*2;
 const unsigned nt = 2 * M_PI / delta_t, nr = (rout - rin) / delta_r, nz = z_max/delta_z;
-const double rr = 25;
-
+const unsigned fnc = 8, fns = 24;
 
 int main() {
 	cout << "nz = " << nz << endl;
@@ -18,7 +18,8 @@ int main() {
 	FILE *fscad;
     fout = fopen("mesh_v.pov","w");
     fscad = fopen("mesh.scad","w");
-    fprintf(fscad, "module cylinder_ep(p1,p2,r1,r2){\nvector=[p2[0]-p1[0],p2[1]-p1[1],p2[2]-p1[2]];\ndistance=sqrt(pow(vector[0],2)+pow(vector[1],2)+pow(vector[2],2));\ntranslate(vector/2+p1)\nrotate([0,0,atan2(vector[1],vector[0])])\nrotate([0,atan2(sqrt(pow(vector[0], 2)+pow(vector[1], 2)),vector[2]), 0])\ncylinder(h=distance,r1=r1,r2=r2,center=true);\n}\nmodule sphere_at(p,r){\ntranslate(p)\nsphere(r);\n}\n");
+    fprintf(fscad, "module cylinder_ep(p1,p2,r,fn){\nvector=[p2[0]-p1[0],p2[1]-p1[1],p2[2]-p1[2]];\ndistance=sqrt(pow(vector[0],2)+pow(vector[1],2)+pow(vector[2],2));\ntranslate(vector/2+p1)\nrotate([0,0,atan2(vector[1],vector[0])])\nrotate([0,atan2(sqrt(pow(vector[0], 2)+pow(vector[1], 2)),vector[2]), 0])\ncylinder(h=distance,r1=r,r2=r,center=true,$fn=fn);\n}\nmodule sphere_at(p,r,fn){\ntranslate(p)\nsphere(r,center=true,$fn=fn);\n}\n");
+    fprintf(fscad, "fnc=%d; fns=%d; rc=%g; rs=%g;\n", fnc, fns, rr, rr);
 	for (unsigned iz = 0; iz < nz; ++iz) {
 		for (unsigned ir = 0; ir < nr; ++ir) {
 			for (unsigned it = 0; it < nt; ++it) {
@@ -42,26 +43,26 @@ int main() {
 					fprintf(fout,"cylinder{<%g,%g,%g>,<%g,%g,%g>,r}\n", x1, y1, z1, x2, y2, z2);
 					fprintf(fout,"cylinder{<%g,%g,%g>,<%g,%g,%g>,r}\n", x2, y2, z2, x3, y3, z3);
 					//
-					fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],%g,%g);\n", x1, y1, z1, x2, y2, z2, rr, rr);
-					fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],%g,%g);\n", x2, y2, z2, x3, y3, z3, rr, rr);
+					fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],rc,fnc);\n", x1, y1, z1, x2, y2, z2);
+					fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],rc,fnc);\n", x2, y2, z2, x3, y3, z3);
 				} else {
 					fprintf(fout,"sphere{<%g,%g,%g>,r}\n", x1, y1, z1);
-					fprintf(fscad,"sphere_at([%g,%g,%g],%g);\n", x1, y1, z1, rr);
+					fprintf(fscad,"sphere_at([%g,%g,%g],rs,fns);\n", x1, y1, z1);
 				}
 				fprintf(fout,"cylinder{<%g,%g,%g>,<%g,%g,%g>,r}\n", x1, y1, z1, x3, y3, z3);
-				fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],%g,%g);\n", x1, y1, z1, x3, y3, z3, rr, rr);
+				fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],rc,fnc);\n", x1, y1, z1, x3, y3, z3);
 				if (iz < nz - 1) {
 					fprintf(fout,"cylinder{<%g,%g,%g>,<%g,%g,%g>,r}\n", x1, y1, z1, x4, y4, z4);
 					fprintf(fout,"cylinder{<%g,%g,%g>,<%g,%g,%g>,r}\n", x3, y3, z3, x4, y4, z4);
 					//
-					fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],%g,%g);\n", x1, y1, z1, x4, y4, z4, rr, rr);
-					fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],%g,%g);\n", x3, y3, z3, x4, y4, z4, rr, rr);
+					fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],rc,fnc);\n", x1, y1, z1, x4, y4, z4);
+					fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],rc,fnc);\n", x3, y3, z3, x4, y4, z4);
 					if (ir < nr - 1){
 						fprintf(fout,"cylinder{<%g,%g,%g>,<%g,%g,%g>,r}\n", x2, y2, z2, x5, y5, z5);
 						fprintf(fout,"cylinder{<%g,%g,%g>,<%g,%g,%g>,r}\n", x3, y3, z3, x5, y5, z5);
 						//
-						fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],%g,%g);\n", x2, y2, z2, x5, y5, z5, rr, rr);
-						fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],%g,%g);\n", x3, y3, z3, x5, y5, z5, rr, rr);
+						fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],rc,fnc);\n", x2, y2, z2, x5, y5, z5);
+						fprintf(fscad,"cylinder_ep([%g,%g,%g], [%g,%g,%g],rc,fnc);\n", x3, y3, z3, x5, y5, z5);
 					}
 				}
 			}
